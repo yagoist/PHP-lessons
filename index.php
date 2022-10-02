b2b01677553f025b9b1d778531dd49209c957add
yagoist <yagogo83@gmail.com>
2022-10-02 23:02:23 +0300

    HW4

--- index.php
+++ index.php
@@ -1,82 +1,91 @@
 <?php
 
-// lesson 3, task 1
+//// lesson 4, task 1
 
 
-$arrayOne = [5, 7, 8, 15, 5, 2, 3, 34, 9, 10];
-$arrayTwo = [3, 4, 6, 9, 1, 14, 21, 6, 8, 15];
-$arrayResult = [];
+$arrayOne = [5, 7, 8, 15, 5, 2, 3, 34, 9, 10, 78, 95, 43];
 
-for ($i = 0; $i < count($arrayOne); $i++) {
-        $arrayResult[] = $arrayOne[$i] * $arrayTwo[$i];
-}
-print_r($arrayResult);
+$foo = function ($item) :string
+{
+    return $item % 2 === 0 ? 'четное' : 'нечетное';
+};
 
-
-//// lesson 3, task 2
+var_dump(array_map($foo, $arrayOne));
 
 
-$userName = readline('Введите имя именинника.');
-$congratulationsPartOne = ['богатырского здоровья', 'надежных друзей', 'чаще улыбаться', 'держаться огурцом', 'не быть помидором', 'не двинуть кони'];
-$congratulationsPartTwo = ['с работы не выгнали', 'денег побольше', 'богатств несметных', 'жена любила', 'ноги носили', 'руки таскали'];
+//// lesson 4, task 2
 
-$randomCongratulation = [
-    0 => $congratulationsPartOne[array_rand($congratulationsPartOne)],
-    1 => $congratulationsPartTwo[array_rand($congratulationsPartTwo)]
+
+$arrayOfInteger = [51, 3, 43, 8, 19, 54, 10, 48, 99, 103, 61];
+
+function findMaxMinAvgInt (array $arr) :array
+{
+    $minInteger = $arr[0];
+    $maxInteger = $arr[0];
+    $sumInteger = 0;
+    foreach ($arr as $item) {
+        if ($item < $minInteger) $minInteger = $item;
+        if ($item > $maxInteger) $maxInteger = $item;
+        $sumInteger += $item;
+    }
+    return [$maxInteger, $minInteger, ($sumInteger / count($arr))];
+};
+
+var_dump(findMaxMinAvgInt($arrayOfInteger));
+
+
+//// lesson 4, task 3
+
+
+$box = [
+  [
+      0 => 'Тетрадь',
+      1 => 'Книга',
+      2 => 'Настольная игра',
+      3 => [
+          'Настольная игра',
+          'Настольная игра'
+      ],
+      4 => [
+          [
+              'Ноутбук',
+              'Зарядное устройство'
+          ],
+          [
+              'Компьютерная мышь',
+              'Набор проводов',
+              [
+                  'Фотография',
+                  'Картина'
+              ]
+          ],
+          [
+              'Иструкция',
+              [
+                  'Ключ'
+              ]
+          ]
+      ]
+  ],
+  [
+      0 => 'Пакет кошачего корма',
+      1 => [
+          'Музыкальный плеер',
+          'Книга'
+      ]
+  ]
 ];
 
-echo('Дорогой '.$userName.', от всего сердца поздравляю тебя с днем рождения, желаю '.(implode(', и чтобы ', $randomCongratulation).'!!!'));
+$answer = false;
 
-
-//// lesson 3, task 3
-
-$students = [
-    'ИТ20' => [
-        'Иванов Иван' => 5,
-        'Кириллов Кирилл' => 3,
-        'Сопляков Виктор' => 4,
-        'Кишкин Никита' => 3,
-        'Воротников Николай' => 3,
-        'Сепультура Ольга' => 5,
-        'Заборов Александр' => 2,
-        'Жигули Виталий' => 3,
-        'Лошадиный Сергей' => 3,
-        'Красный Александр' => 5,
-    ],
-    'БАП20' => [
-        'Никитин Никита' => 4,
-        'Дмитриев Дмитрий' => 4,
-        'Конюшкин Виктор' => 4,
-        'Закидышев Никита' => 1,
-        'Пузатов Николай' => 4,
-        'Слипнотова Ольга' => 2,
-        'Парадный Александр' => 5,
-        'Москвич Виталий' => 3,
-        'Зайцев Сергей' => 5,
-        'Синий Александр' => 2,
-    ]
-];
-
-$averageGradeTotal = 0;
-$nameOfBestGroup = '';
-$studentsForOut = $students;
-
-foreach ($students as $indexGroup => $group) {
-        $averageGrade = 0;
-        $sumGrade = 0;
-    foreach ($group as $student => $grade) {
-            $sumGrade += $grade;
-            if ($grade > 2) {
-                unset($studentsForOut[$indexGroup][$student]);
-            }
-        }
-    $averageGrade = $sumGrade / count($group);
-     if ($averageGrade > $averageGradeTotal) {
-         $averageGradeTotal=$averageGrade;
-         $nameOfBestGroup = $indexGroup;
-     }
-
+function searchItem (string $itemName, array $arrayOfItems): bool
+{
+    global $answer;
+    foreach ($arrayOfItems as $item) {
+        if ($itemName === $item) $answer = true;
+        if (gettype($item) ===  'array') searchItem($itemName, $item);
+  }
+  return $answer;
 }
-echo ("Наибольшая средняя оценка составляет: $averageGradeTotal в группе: $nameOfBestGroup").PHP_EOL;
-print_r($studentsForOut); //вывод на отчисление
 
+var_dump(searchItem('Ключ', $box));
